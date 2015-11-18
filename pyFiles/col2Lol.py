c1786aa95d362a29a324fcfef204df636076f4e4
Tyler Olivieri <tue62960@electrodata.(none)>
2015-11-18 13:08:07 -0500

    update col2Lol

--- pyFiles/col2Lol.py
+++ pyFiles/col2Lol.py
@@ -5,6 +5,7 @@
   #takes input of fname, which contains columns in txt file
   #takes delimeter char that seperates the column in file
   #recreates table in a list of lists
+  #removes first column (header)
   #col2LoL will return a list of lists (2x2 array)
 ##
 
@@ -15,22 +16,18 @@ def col2Lol(fname, delim):
     #to correctly create list of list size
     #
     with open(fname, "r") as fp:
-
-        #loop over file
+        
+        #read a line
         #
-        for line in fp:
-
-            #split line by delimeter
-            #
-            line = line.split(delim)
-
-            #find number of columns
-            #
-            colAmt = len(line)
-
-            #break out of loop
-            #
-            break
+        line = fp.readline()
+        
+        #split the line by delim into columns
+        #
+        line = line.split(delim)
+        
+        #find number of columns in file
+        #
+        colAmt = len(line)
 
     #delcare list of lists
     #
@@ -48,34 +45,40 @@ def col2Lol(fname, delim):
             #split line by delimeter
             #
             splitLine = line.split(delim)
-
+            
             #fill list of lists by appending each column in line to
             #appropriate list column
             #loop over line
             #
             for i in range(colAmt):
-
+                
                 #append each string to each list creating the same columns as in txt file
                 #
                 twoBy2[i].append(splitLine[i])
-
+ 
+    #remove the first entry in every list
+    #
+    for j in range(colAmt):
+        twoBy2[j].pop(0)
+    #return the list
+    #
     return twoBy2
 
-#import sys
+import sys
 
 #main for testing
-#
-#def main(argv):
+
+def main(argv):
     
-    #fname = argv[1]
-    #delim = "|"
-    #listoflists = col2Lol(fname, delim)
+    fname = argv[1]
+    delim = "|"
+    listoflists = col2Lol(fname, delim)
     
-    #print(listoflists)
+    print(listoflists)
     
 
     
 #end gracefully
 #
-#if __name__ == "__main__":
-    #main(sys.argv[0:])
+if __name__ == "__main__":
+    main(sys.argv[0:])
